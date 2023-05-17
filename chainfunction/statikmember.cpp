c4b8103d72f083af07889394c01558ea7edcc502
badai ngasubala <badaingasubala12@gmail.com>
2023-05-17 19:40:53 +0700

    Add project files.

---
+++ chainfunction/statikmember.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Lia kurnia");
+	mahasiswa mhs2("Asroni");
+	mhs2.nim = 10;
+	mahasiswa mhs3("Andi kurniawan");
+	mhs3.nim = 20;
+	mahasiswa mhs4("Joko purbo");
+
+	mhs1.printAll(); \
+		mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
+}
